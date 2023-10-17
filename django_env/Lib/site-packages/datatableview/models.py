71d6606ce2f2cee14b11e3f889e0a2b3220d2cf5
samilson <90565699+samilson23@users.noreply.github.com>
2023-10-17 09:55:02 +0300

    Done

---
+++ django_env/Lib/site-packages/datatableview/models.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+# Required for Django to recognize this app
---
+++ django_env/Lib/site-packages/datatableview/tests/testcase.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase
+from django.core.management import call_command
+from django.apps import apps
+
+
+class DatatableViewTestCase(TestCase):
+    def _pre_setup(self):
+        """
+        Asks the management script to re-sync the database.  Having test-only models is a pain.
+        """
+        apps.clear_cache()
+        call_command("migrate", interactive=False, verbosity=0)
+        call_command("loaddata", "initial_data", verbosity=0)
+        super(DatatableViewTestCase, self)._pre_setup()
