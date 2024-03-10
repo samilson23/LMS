6102b28fc7f27962308b622494ff44739a19104c
samilson23 <samwelmilson23@gmail.com>
2024-03-10 15:10:37 +0300

    commit

--- Pesapal/pesapal_ops.py
+++ Pesapal/pesapal_ops.py
@@ -2,9 +2,9 @@ from . import pesapal_processor
 
 
 def post_transaction(Reference, FirstName, LastName, Email, PhoneNumber, Description, Amount, Type):
-    oauth_consumer_key = "nRZXsEDVT2uMetIZxnusQPOA4zHyBtlV"
-    oauth_consumer_secret = "UzKwzyN9GbXgSNC8HKcMHrJusME="
-    oauth_callback = "http://767e1295.ngrok.io/payment/oauth_callback/"
+    oauth_consumer_key = "qkio1BGGYAXTu2JOfm7XSXNruoZsrqEW"
+    oauth_consumer_secret = "osGQ364R49cXKeOYSpaOnT++rHs="
+    oauth_callback = 'https://samilsonuniversity1-5c5d6f1676ed.herokuapp.com/Student/CompleteTransaction'
 
     pesapal = pesapal_processor.PesaPal(oauth_consumer_key, oauth_consumer_secret, 'sandbox')
     pesapal.reference = Reference
